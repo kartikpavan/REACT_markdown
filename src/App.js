0e8afa8f585c5b72ae24d2f93c5e729faf19f4fb
kartikpavan <kartik.varanasi@yahoo.com>
2022-03-05 22:08:01 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-
+import ReactMarkdown from 'react-markdown';
 function App() {
   const [markdown, setMarkdown] = React.useState('');
 
   return (
     <main>
       <section className="markdown">
-        <textarea className="input"></textarea>
+        <textarea
+          className="input"
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+        />
+        <article className="result">
+          <h1> MarkDown Preview</h1>
+          <hr />
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </article>
       </section>
     </main>
   );
